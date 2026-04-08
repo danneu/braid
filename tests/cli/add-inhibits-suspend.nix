03b3bbf96530f836836fd314b29d88d7769b76f1
Dan Neumann <danrodneu@gmail.com>
2026-04-08 16:04:07 -0500

    double test disk size
    couldn't reproduce, but i think i saw a failure on silverstone that
    might get fixed by this. don't remember.

--- tests/cli/add-inhibits-suspend.nix
+++ tests/cli/add-inhibits-suspend.nix
@@ -19,25 +19,31 @@
 {
   name = "add-inhibits-suspend";
 
-  nodes.machine = { pkgs, ... }: {
-    virtualisation.emptyDiskImages = [
-      { size = 1024; driveConfig.deviceExtraOpts.serial = "disk1"; }
-      { size = 1024; driveConfig.deviceExtraOpts.serial = "disk2"; }
-    ];
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      virtualisation.emptyDiskImages = [
+        {
+          size = 2048;
+          driveConfig.deviceExtraOpts.serial = "disk1";
+        }
+        {
+          size = 2048;
+          driveConfig.deviceExtraOpts.serial = "disk2";
+        }
+      ];
 
-    environment.systemPackages = [
-      braid
-      pkgs.cryptsetup
-      pkgs.btrfs-progs
-    ];
+      environment.systemPackages = [
+        braid
+        pkgs.cryptsetup
+        pkgs.btrfs-progs
+      ];
 
-    environment.etc."braid/config.json".text = builtins.toJSON {
-      mount_point = "/mnt/storage";
+      environment.etc."braid/config.json".text = builtins.toJSON {
+        mount_point = "/mnt/storage";
+      };
     };
-  };
 
   testScript =
-    builtins.readFile ./inhibitor_helpers.py
-    + "\n\n"
-    + builtins.readFile ./add-inhibits-suspend.py;
+    builtins.readFile ./inhibitor_helpers.py + "\n\n" + builtins.readFile ./add-inhibits-suspend.py;
 }
